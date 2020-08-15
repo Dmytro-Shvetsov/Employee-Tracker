4c1391d57fdfdf21274aa051f16f81641c0709e2
Dmytro-Shvetsov <shvetsovdi2@gmail.com>
2020-08-15 22:53:07 +0300

    Make csrf token httponly

--- ETWeb/frontend/src/services/configuredAxiosInstance.js
+++ ETWeb/frontend/src/services/configuredAxiosInstance.js
@@ -1,21 +1,8 @@
 import axios from 'axios';
 
-function getCookie(cname) {
-     const name = cname + "=";
-     const ca = document.cookie.split(';');
-     for(let i = 0; i < ca.length; i++) {
-         let c = ca[i];
-         while (c.charAt(0) === ' ')
-             c = c.substring(1);
-         if (c.indexOf(name) === 0)
-             return c.substring(name.length, c.length);
-     }
-     return '';
-}
-
 const globalConfig = {
     headers: {
-        'X-CSRFToken': getCookie('csrftoken'),
+        'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
         'Content-Type': 'application/json',
     }
 };
