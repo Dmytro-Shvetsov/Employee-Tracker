e1db6c50c9abc4bc43be1feea43b8e2ba3c6a803
Dmytro-Shvetsov <shvetsovdi2@gmail.com>
2020-08-26 17:32:04 +0300

    Configure Configure settings for web application deployment

--- ETWeb/ETWeb/routing.py
+++ ETWeb/ETWeb/routing.py
@@ -1,11 +1,8 @@
-import os
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from accounts.authentication import TokenAuthMiddlewareStack
 from .consumers import AsyncClientConnectionsConsumer, AsyncManagerConnectionsConsumer
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ETWeb.settings')
 
 channel_routing = ProtocolTypeRouter({
     "websocket": TokenAuthMiddlewareStack(
