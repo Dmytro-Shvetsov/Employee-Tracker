3efba1702f236c81357631409e40b8c7d058792f
Dmytro-Shvetsov <shvetsovdi2@gmail.com>
2020-08-30 15:20:54 +0300

    Minor improvements

--- ETWeb/ETWeb/urls.py
+++ ETWeb/ETWeb/urls.py
@@ -11,9 +11,11 @@ urlpatterns = [
     path('api/contact/', ContactView.as_view()),
 ]
 
-# if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    print('Static files are served with Whitenoise.')
 
 urlpatterns += [
     # Pages
